220082d5e4891255d813fa7da30431a2756e1db2
SO Pulatov <SirotilloPulatov@gmail.com>
2021-10-27 09:46:54 +0500

    added section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+// let gridView = document.querySelector('grid-view')
+let listView = document.querySelector('.product-blocks');
+let gridView = document.querySelector('.grid-cards');
+
+
+function gridbtn() {
+  listView.classList.add('hide-blocks');
+  gridView.classList.add('show-grid');
+}
+
+function listbtn() {
+  listView.classList.remove('hide-blocks');
+  gridView.classList.remove('show-grid');
+}
+
